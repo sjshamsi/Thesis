fc50eb698102b7d1740bb9f583042dc82cdf5868
SJShamsi <sjs.shamsi@gmail.com>
2025-01-24 02:14:44 +0000

    calculated new hists

--- dmdt_Analysis/dmdt_functions.py
+++ dmdt_Analysis/dmdt_functions.py
@@ -1,8 +1,8 @@
-from numpy import subtract, triu_indices_from, log10, histogram2d
+from numpy import subtract, triu_indices_from, log10, histogram2d, linspace
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-def get_differenciation(magnitudes, times, log_dt=True):
+def get_differenciation(magnitudes, times, log_dt=False):
     """Returns the 'differenciation' when given an array of magnitudes and corresponding times.
     (According to Mahabal et. al. 2017)
 
@@ -68,31 +68,91 @@ def get_2Dhistogram(dmagnitudes, dmagnitudes_bins, dtimes, dtimes_bins, normalis
     return hist, _dm_edges, _dt_edges, 
 
 
-def plot_dm_dt(dm_dt_hist, dm_edges, dt_edges, title=None, *kwargs):
-    """Plots the provided histogram.
+def plot_dm_dt(dm_dt_hist, band, dm_bins, dt_bins, dm_nticks=10, dt_nticks=10, title=None):
+    dm_indices = linspace(0, len(dm_bins) - 1, dm_nticks, dtype=int)
+    dt_indices = linspace(0, len(dt_bins) - 1, dt_nticks, dtype=int)
 
-    Parameters
-    ----------
-    dm_dt_hist : numpy.ndarray
-        The 2D image to be plotted.
-    dm_edges : numpy.ndarray
-        dm   bin edges
-    dt_edges : numpy.ndarray
-        dt bin edges
-    """
-    plt.imshow(dm_dt_hist, origin='lower', cmap='viridis', aspect='auto', extent=[dt_edges[0], dt_edges[-1], dm_edges[0], dm_edges[-1]], *kwargs)
-    plt.colorbar(label=f'Count Intensity ({dm_dt_hist.min()}-{dm_dt_hist.max()})')
-    
-    if not title:
-        title = "2D Binned Representation of $\Delta$t vs. $\Delta$mag"
-    plt.title(title)
-    plt.xlabel(r'$\log_{10} \; \Delta$t (days)')
-    plt.ylabel(r'$\Delta$mag')
+    dm_ticks_labels = []
+    for index in dm_indices:
+        formatted_string = f"{dm_bins[index]:.2f}"
+        dm_ticks_labels.append(formatted_string)
+
+    dt_ticks_labels = []
+    for index in dt_indices:
+        formatted_string = f"{dt_bins[index]:.1e}"
+        base, exponent = formatted_string.split('e')
+        dt_ticks_labels.append(f"${base} \\times 10^{{{int(exponent)}}}$")
+
+    if len(band) == 2:
+        fig_size = (12, 8)
+        alpha = 0.5
+    else:
+        fig_size = (10, 8)
+        alpha = 1
+
+    fig, ax = plt.subplots(figsize=fig_size)
+
+    if 'r' in band:
+        r_ch = ax.imshow(dm_dt_hist[:, :, 0], origin='lower', cmap='Reds', aspect='auto', extent=[0, len(dt_bins)-1, 0, len(dm_bins) - 1], alpha=alpha)
+        r_cbar = plt.colorbar(r_ch, ax=ax, shrink=0.8, label="$r$ band")
+    if 'g' in band:
+        g_ch = ax.imshow(dm_dt_hist[:, :, 1], origin='lower', cmap='Greens', aspect='auto', extent=[0, len(dt_bins)-1, 0, len(dm_bins) - 1], alpha=alpha)
+        g_cbar = plt.colorbar(g_ch, ax=ax, shrink=0.8, label="$g$ band", location='left')
+
+    plt.yticks(ticks=dm_indices, labels=dm_ticks_labels)
+    plt.xticks(ticks=dt_indices, labels=dt_ticks_labels, size=7)
+
+    plt.xlabel(f'{dt_bins.min():.2f} $< dt <$ {dt_bins.max():.2f} (days) {len(dt_bins) - 1} bins', size=12)
+    plt.ylabel(f'{dm_bins.min():.2f} $< dm <$ {dm_bins.max():.2f} (magnitude) {len(dm_bins) - 1} bins', size=12)
+
+    if title:
+        fig.suptitle(title, size=12)
 
     plt.grid(False)
-
-    plt.legend()
+    fig.tight_layout()
     plt.show()
+
+
+def plot_dm_dt_rgb(dm_dt_hist, dm_bins, dt_bins, dm_nticks=10, dt_nticks=10, title=None):
+    dm_indices = linspace(0, len(dm_bins) - 1, dm_nticks, dtype=int)
+    dt_indices = linspace(0, len(dt_bins) - 1, dt_nticks, dtype=int)
+
+    dm_ticks_labels = []
+    for index in dm_indices:
+        formatted_string = f"{dm_bins[index]:.2f}"
+        dm_ticks_labels.append(formatted_string)
+
+    dt_ticks_labels = []
+    for index in dt_indices:
+        formatted_string = f"{dt_bins[index]:.1e}"
+        base, exponent = formatted_string.split('e')
+        dt_ticks_labels.append(f"${base} \\times 10^{{{int(exponent)}}}$")
+
+    fig, ax = plt.subplots(figsize=(9, 8))
+
+    dm_dt_hist[:, :, 0] = dm_dt_hist[:, :, 0] / dm_dt_hist[:, :, 0].max()
+    dm_dt_hist[:, :, 1] = dm_dt_hist[:, :, 1] / dm_dt_hist[:, :, 1].max()
+
+    ax.imshow(dm_dt_hist, origin='lower', aspect='auto', extent=[0, len(dt_bins)-1, 0, len(dm_bins) - 1])
+    # cbar_red = plt.colorbar(plt.cm.ScalarMappable(cmap='Reds'), ax=ax, shrink=0.8, pad=0.000000000001)
+    # cbar_red.set_label("Red Intensity", rotation=270, labelpad=15)
+
+    # cbar_green = plt.colorbar(plt.cm.ScalarMappable(cmap='Greens'), ax=ax, shrink=0.8, pad=0.01)
+    # cbar_green.set_label("Green Intensity", rotation=270, labelpad=15)
+
+    plt.yticks(ticks=dm_indices, labels=dm_ticks_labels)
+    plt.xticks(ticks=dt_indices, labels=dt_ticks_labels, size=7)
+
+    plt.xlabel(f'{dt_bins.min():.2f} $< dt <$ {dt_bins.max():.2f} (days) {len(dt_bins) - 1} bins', size=12)
+    plt.ylabel(f'{dm_bins.min():.2f} $< dm <$ {dm_bins.max():.2f} (magnitude) {len(dm_bins) - 1} bins', size=12)
+
+    if title:
+        fig.suptitle(title, size=12)
+
+    plt.grid(False)
+    fig.tight_layout()
+    plt.show()
+    
 
 def save_dm_dt_image(dm_dt_hist, name, cmap=cm.viridis):
     """Saves a histogram image as it is shown in plt.imshow()
